ab24808765ab0529fa2fa9257602316fd52ef819
Frederic Mallet <Frederic.Mallet@univ-cotedazur.fr>
2021-06-11 10:54:56 +0200

    Examples for primitives operators

--- Examples/CCSL_Primitives/src-gen/lccsl/LcBoundedCausality.java
+++ Examples/CCSL_Primitives/src-gen/lccsl/LcBoundedCausality.java
@@ -5,6 +5,7 @@ import fr.kairos.timesquare.ccsl.simple.IUtility;
 import fr.kairos.timesquare.ccsl.simple.ISpecificationBuilder;
 import fr.kairos.lightccsl.core.stepper.StepperUtility;
 import fr.unice.lightccsl.sat.bdd.BDDSolutionFinder;
+import fr.kairos.lightccsl.sts.STSJavaBackend;
 import fr.kairos.lightccsl.sts.STSUtility;
 //import fr.kairos.sts.pojo.choco.ChocoInvariantHelper;
 import fr.aoste.sync.ilp.JalinoptInvariantHelper;
@@ -40,8 +41,8 @@ public class LcBoundedCausality implements ISpecificationBuilder {
 		STSUtility sts = new STSUtility();
 		//ChocoInvariantHelper.activate(); // to reduce STS
 		JalinoptInvariantHelper.activate(); // to reduce STS
-		sts.setBackend(new fr.aoste.sync.gen.STStoDOT(), ".dot");
-		sts.setParam("folderName", "sts");
+		sts.setBackend(new STSJavaBackend());
+		sts.setParam("folderName", "src-gen/sts");
 		sts.treat(name, INSTANCE);
 	}
 }
--- Examples/CCSL_Primitives/src-gen/lccsl/Lcprec.java
+++ Examples/CCSL_Primitives/src-gen/lccsl/Lcprec.java
@@ -5,6 +5,7 @@ import fr.kairos.timesquare.ccsl.simple.IUtility;
 import fr.kairos.timesquare.ccsl.simple.ISpecificationBuilder;
 import fr.kairos.lightccsl.core.stepper.StepperUtility;
 import fr.unice.lightccsl.sat.bdd.BDDSolutionFinder;
+import fr.kairos.lightccsl.sts.STSJavaBackend;
 import fr.kairos.lightccsl.sts.STSUtility;
 //import fr.kairos.sts.pojo.choco.ChocoInvariantHelper;
 import fr.aoste.sync.ilp.JalinoptInvariantHelper;
@@ -19,8 +20,10 @@ public class Lcprec implements ISpecificationBuilder {
 	public void build(ISimpleSpecification simple) {
 		simple.addClock("a");
 		simple.addClock("b");
+		simple.addClock("c");
 		
 		simple.precedence("a", "b");
+		simple.causality("b", "c");
 	}
 	private static IUtility[] utilities = { 
 		new fr.kairos.timesquare.ccsl.simple.PrettyPrintUtility()
@@ -40,8 +43,8 @@ public class Lcprec implements ISpecificationBuilder {
 		STSUtility sts = new STSUtility();
 		//ChocoInvariantHelper.activate(); // to reduce STS
 		JalinoptInvariantHelper.activate(); // to reduce STS
-		sts.setBackend(new fr.aoste.sync.gen.STStoDOT(), ".dot");
-		sts.setParam("folderName", "sts");
+		sts.setBackend(new STSJavaBackend());
+		sts.setParam("folderName", "src-gen/sts");
 		sts.treat(name, INSTANCE);
 	}
 }
