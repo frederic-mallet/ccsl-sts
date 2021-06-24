e7c5781acdf62e2b9d7e059214c6c9ac5c4d4de5
Frederic Mallet <Frederic.Mallet@univ-cotedazur.fr>
2021-06-24 23:41:34 +0200

    Add an example of sampled on

---
+++ Examples/CCSL_Primitives/doc/SampledOn.md
@@ -0,0 +1,14 @@
+# The Clock Constraint Specification Language
+
+[Main](../Readme.md)
+
+## SampledOn
+
+[src](../lc/SampledOn.lc) [simul](../vcd/SampledOn.html)
+
+- **SampledOn** synchronizes a clock **a** on a clock **s**.
+
+<center>
+<img alt="sampledOn" src="../sts/SampledOn.png" height = "200px"/><br>
+<strong>a = b $ 1 on s </strong> a is b sampled on s
+</center>
--- Examples/CCSL_Primitives/src-gen/lccsl/LcSampledOn.java
+++ Examples/CCSL_Primitives/src-gen/lccsl/LcSampledOn.java
@@ -4,15 +4,15 @@ import fr.kairos.timesquare.ccsl.ISimpleSpecification;
 import fr.kairos.timesquare.ccsl.simple.IUtility;
 import fr.kairos.timesquare.ccsl.simple.ISpecificationBuilder;
 import fr.kairos.lightccsl.core.stepper.StepperUtility;
-import fr.unice.lightccsl.sat.bdd.BDDSolutionFinder;
+import fr.kairos.lightccsl.sts.STSSolutionFinder;
 import fr.kairos.lightccsl.sts.STSUtility;
 //import fr.kairos.sts.pojo.choco.ChocoInvariantHelper;
 import fr.aoste.sync.ilp.JalinoptInvariantHelper;
 import fr.kairos.timesquare.ccsl.reduce.ReduceSpecificationBuilder;
 
-public class Lcprec implements ISpecificationBuilder {
-	static public Lcprec INSTANCE = new Lcprec();
-	private Lcprec () {
+public class LcSampledOn implements ISpecificationBuilder {
+	static public LcSampledOn INSTANCE = new LcSampledOn();
+	private LcSampledOn () {
 		// SINGLETON
 	}	
 
@@ -20,26 +20,24 @@ public class Lcprec implements ISpecificationBuilder {
 	public void build(ISimpleSpecification simple) {
 		simple.addClock("a");
 		simple.addClock("b");
-		simple.addClock("c");
+		simple.addClock("s");
 		
-		simple.precedence("a", "b");
-		simple.causality("b", "c");
+		simple.delayFor("a", "b", 1, -1, "s");
 	}
 	private static IUtility[] utilities = { 
 		new fr.kairos.timesquare.ccsl.simple.PrettyPrintUtility()
 	};
 	public static void main(String[] args) {
-		String name = "prec";
+		String name = "SampledOn";
 		
-		ReduceSpecificationBuilder INSTANCE = new ReduceSpecificationBuilder(Lcprec.INSTANCE);
+		ReduceSpecificationBuilder INSTANCE = new ReduceSpecificationBuilder(LcSampledOn.INSTANCE);
 		for (IUtility u : utilities) {
 			u.treat(name, INSTANCE);
 		}
 		
-		StepperUtility exe = new StepperUtility(new BDDSolutionFinder());
-		exe.setParam(StepperUtility.INTERACTIVE, false);
+		StepperUtility exe = new StepperUtility(new STSSolutionFinder());
+		exe.setParam(StepperUtility.INTERACTIVE, true);
 		exe.setBackend(new fr.unice.lightccsl.html.HtmlVCDBackend());
-		exe.setParam(StepperUtility.NB_STEPS, 20);
 		exe.treat(name, INSTANCE);
 		
 		STSUtility sts = new STSUtility();
