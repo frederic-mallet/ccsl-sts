18d1516165b1f308416c34fec386991fb56dc6fe
Frederic Mallet <Frederic.Mallet@univ-cotedazur.fr>
2021-07-08 10:32:02 +0200

    subclocking

---
+++ Examples/CCSL_Primitives/src-gen/lccsl/Lcsubclocking.java
@@ -0,0 +1,46 @@
+package lccsl;
+
+import fr.kairos.timesquare.ccsl.ISimpleSpecification;
+import fr.kairos.timesquare.ccsl.simple.IUtility;
+import fr.kairos.timesquare.ccsl.simple.ISpecificationBuilder;
+import fr.kairos.lightccsl.sts.STSJavaBackend;
+import fr.kairos.lightccsl.sts.STSUtility;
+//import fr.kairos.sts.pojo.choco.ChocoInvariantHelper;
+import fr.aoste.sync.ilp.JalinoptInvariantHelper;
+import fr.kairos.timesquare.ccsl.reduce.ReduceSpecificationBuilder;
+
+public class Lcsubclocking implements ISpecificationBuilder {
+	static public Lcsubclocking INSTANCE = new Lcsubclocking();
+	private Lcsubclocking () {
+		// SINGLETON
+	}	
+
+	@Override
+	public void build(ISimpleSpecification simple) {
+		simple.addClock("a");
+		simple.addClock("b");
+		simple.addClock("c");
+		
+		simple.subclock("a", "b");
+		simple.subclock("b", "c");
+	}
+	private static IUtility[] utilities = { 
+		new fr.kairos.timesquare.ccsl.simple.PrettyPrintUtility()
+	};
+	public static void main(String[] args) {
+		String name = "subclocking";
+		
+		ReduceSpecificationBuilder INSTANCE = new ReduceSpecificationBuilder(Lcsubclocking.INSTANCE);
+		for (IUtility u : utilities) {
+			u.treat(name, INSTANCE);
+		}
+		// no execution
+		
+		STSUtility sts = new STSUtility();
+		//ChocoInvariantHelper.activate(); // to reduce STS
+		JalinoptInvariantHelper.activate(); // to reduce STS
+		sts.setBackend(new STSJavaBackend());
+		sts.setParam("folderName", "src-gen/sts");
+		sts.treat(name, INSTANCE);
+	}
+}
