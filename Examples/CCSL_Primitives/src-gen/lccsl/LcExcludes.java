054a882acb89082844a8f431a9f4b459e6807693
Frederic Mallet <Frederic.Mallet@univ-cotedazur.fr>
2021-07-08 18:38:43 +0200

    Test exclusion

---
+++ Examples/CCSL_Primitives/src-gen/lccsl/LcExcludes.java
@@ -0,0 +1,53 @@
+package lccsl;
+
+import fr.kairos.timesquare.ccsl.ISimpleSpecification;
+import fr.kairos.timesquare.ccsl.simple.IUtility;
+import fr.kairos.timesquare.ccsl.simple.ISpecificationBuilder;
+import fr.kairos.lightccsl.core.stepper.StepperUtility;
+import fr.unice.lightccsl.sat.bdd.BDDSolutionFinder;
+import fr.kairos.lightccsl.sts.STSUtility;
+//import fr.kairos.sts.pojo.choco.ChocoInvariantHelper;
+import fr.aoste.sync.ilp.JalinoptInvariantHelper;
+import fr.kairos.timesquare.ccsl.reduce.ReduceSpecificationBuilder;
+
+public class LcExcludes implements ISpecificationBuilder {
+	static public LcExcludes INSTANCE = new LcExcludes();
+	private LcExcludes () {
+		// SINGLETON
+	}	
+
+	@Override
+	public void build(ISimpleSpecification simple) {
+		simple.addClock("a");
+		simple.addClock("b");
+		simple.addClock("c");
+		simple.addClock("d");
+		
+		simple.exclusion("a", "b");
+		simple.exclusion("a", "c");
+		simple.exclusion("b", "c");
+	}
+	private static IUtility[] utilities = { 
+		new fr.kairos.timesquare.ccsl.simple.PrettyPrintUtility()
+	};
+	public static void main(String[] args) {
+		String name = "Excludes";
+		
+		ReduceSpecificationBuilder INSTANCE = new ReduceSpecificationBuilder(LcExcludes.INSTANCE);
+		for (IUtility u : utilities) {
+			u.treat(name, INSTANCE);
+		}
+		
+		StepperUtility exe = new StepperUtility(new BDDSolutionFinder());
+		exe.setParam(StepperUtility.INTERACTIVE, true);
+		exe.setBackend(new fr.unice.lightccsl.html.HtmlVCDBackend());
+		exe.treat(name, INSTANCE);
+		
+		STSUtility sts = new STSUtility();
+		//ChocoInvariantHelper.activate(); // to reduce STS
+		JalinoptInvariantHelper.activate(); // to reduce STS
+		sts.setBackend(new fr.aoste.sync.gen.STStoDOT(), ".dot");
+		sts.setParam("folderName", "sts");
+		sts.treat(name, INSTANCE);
+	}
+}
