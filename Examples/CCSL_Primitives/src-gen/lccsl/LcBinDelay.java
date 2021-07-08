28b2b778f5dec6771ffab7858efbd1ec0b78f412
Frederic Mallet <Frederic.Mallet@univ-cotedazur.fr>
2021-07-08 19:19:28 +0200

    More primitives, examples and debug

--- Examples/CCSL_Primitives/src-gen/lccsl/LcBinDelay.java
+++ Examples/CCSL_Primitives/src-gen/lccsl/LcBinDelay.java
@@ -3,7 +3,8 @@ package lccsl;
 import fr.kairos.timesquare.ccsl.ISimpleSpecification;
 import fr.kairos.timesquare.ccsl.simple.IUtility;
 import fr.kairos.timesquare.ccsl.simple.ISpecificationBuilder;
-import fr.kairos.lightccsl.sts.STSJavaBackend;
+import fr.kairos.lightccsl.core.stepper.StepperUtility;
+import fr.unice.lightccsl.sat.bdd.BDDSolutionFinder;
 import fr.kairos.lightccsl.sts.STSUtility;
 //import fr.kairos.sts.pojo.choco.ChocoInvariantHelper;
 import fr.aoste.sync.ilp.JalinoptInvariantHelper;
@@ -31,13 +32,17 @@ public class LcBinDelay implements ISpecificationBuilder {
 		for (IUtility u : utilities) {
 			u.treat(name, INSTANCE);
 		}
-		// no execution
+		
+		StepperUtility exe = new StepperUtility(new BDDSolutionFinder());
+		exe.setParam(StepperUtility.INTERACTIVE, true);
+		exe.setBackend(new fr.unice.lightccsl.html.HtmlVCDBackend());
+		exe.treat(name, INSTANCE);
 		
 		STSUtility sts = new STSUtility();
 		//ChocoInvariantHelper.activate(); // to reduce STS
 		JalinoptInvariantHelper.activate(); // to reduce STS
-		sts.setBackend(new STSJavaBackend());
-		sts.setParam("folderName", "src-gen/sts");
+		sts.setBackend(new fr.aoste.sync.gen.STStoDOT(), ".dot");
+		sts.setParam("folderName", "sts");
 		sts.treat(name, INSTANCE);
 	}
 }
