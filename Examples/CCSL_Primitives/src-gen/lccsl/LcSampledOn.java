4b182bb329910846854c1f3f25f2dbf0487df51a
Frederic Mallet <Frederic.Mallet@univ-cotedazur.fr>
2021-07-08 11:11:15 +0200

    Use new syntax for sampledOn

--- Examples/CCSL_Primitives/src-gen/lccsl/LcSampledOn.java
+++ Examples/CCSL_Primitives/src-gen/lccsl/LcSampledOn.java
@@ -3,8 +3,7 @@ package lccsl;
 import fr.kairos.timesquare.ccsl.ISimpleSpecification;
 import fr.kairos.timesquare.ccsl.simple.IUtility;
 import fr.kairos.timesquare.ccsl.simple.ISpecificationBuilder;
-import fr.kairos.lightccsl.core.stepper.StepperUtility;
-import fr.unice.lightccsl.sat.bdd.BDDSolutionFinder;
+import fr.kairos.lightccsl.sts.STSJavaBackend;
 import fr.kairos.lightccsl.sts.STSUtility;
 //import fr.kairos.sts.pojo.choco.ChocoInvariantHelper;
 import fr.aoste.sync.ilp.JalinoptInvariantHelper;
@@ -34,17 +33,13 @@ public class LcSampledOn implements ISpecificationBuilder {
 		for (IUtility u : utilities) {
 			u.treat(name, INSTANCE);
 		}
-		
-		StepperUtility exe = new StepperUtility(new BDDSolutionFinder());
-		exe.setParam(StepperUtility.INTERACTIVE, true);
-		exe.setBackend(new fr.unice.lightccsl.html.HtmlVCDBackend());
-		exe.treat(name, INSTANCE);
+		// no execution
 		
 		STSUtility sts = new STSUtility();
 		//ChocoInvariantHelper.activate(); // to reduce STS
 		JalinoptInvariantHelper.activate(); // to reduce STS
-		sts.setBackend(new fr.aoste.sync.gen.STStoDOT(), ".dot");
-		sts.setParam("folderName", "sts");
+		sts.setBackend(new STSJavaBackend());
+		sts.setParam("folderName", "src-gen/sts");
 		sts.treat(name, INSTANCE);
 	}
 }
