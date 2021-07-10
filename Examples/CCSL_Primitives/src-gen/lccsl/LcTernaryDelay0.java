ecc5ea2ee69621a9707d56f7c34708f26a2c5f47
Frederic Mallet <Frederic.Mallet@univ-cotedazur.fr>
2021-07-10 13:02:10 +0200

    Should not reduce this example or will be empty

--- Examples/CCSL_Primitives/src-gen/lccsl/LcTernaryDelay0.java
+++ Examples/CCSL_Primitives/src-gen/lccsl/LcTernaryDelay0.java
@@ -6,6 +6,7 @@ import fr.kairos.timesquare.ccsl.simple.ISpecificationBuilder;
 import fr.kairos.lightccsl.sts.STSUtility;
 //import fr.kairos.sts.pojo.choco.ChocoInvariantHelper;
 import fr.aoste.sync.ilp.JalinoptInvariantHelper;
+import fr.kairos.timesquare.ccsl.reduce.ReduceSpecificationBuilder;
 
 public class LcTernaryDelay0 implements ISpecificationBuilder {
 	static public LcTernaryDelay0 INSTANCE = new LcTernaryDelay0();
@@ -26,7 +27,7 @@ public class LcTernaryDelay0 implements ISpecificationBuilder {
 	public static void main(String[] args) {
 		String name = "TernaryDelay0";
 		
-		// do not reduce
+		ReduceSpecificationBuilder INSTANCE = new ReduceSpecificationBuilder(LcTernaryDelay0.INSTANCE);
 		for (IUtility u : utilities) {
 			u.treat(name, INSTANCE);
 		}
