4253070d6b423732b0a508e25957d0c852b74db7
Frederic Mallet <Frederic.Mallet@univ-cotedazur.fr>
2023-05-06 19:43:54 +0200

    remove soft lock in modes

--- Examples/Brains/src-gen/lccsl/Lcmodes.java
+++ Examples/Brains/src-gen/lccsl/Lcmodes.java
@@ -5,6 +5,7 @@ import fr.kairos.timesquare.ccsl.simple.IUtility;
 import fr.kairos.timesquare.ccsl.simple.ISpecificationBuilder;
 import fr.kairos.lightccsl.core.stepper.StepperUtility;
 import fr.unice.lightccsl.sat.bdd.BDDSolutionFinder;
+import fr.kairos.lightccsl.sts.STSJavaBackend;
 import fr.kairos.lightccsl.sts.STSUtility;
 //import fr.kairos.sts.pojo.choco.ChocoInvariantHelper;
 import fr.aoste.sync.ilp.JalinoptInvariantHelper;
@@ -22,14 +23,14 @@ public class Lcmodes implements ISpecificationBuilder {
 		simple.addClock("Mode2");
 		simple.addClock("Trigger");
 		simple.addClock("ReactionTime");
+		simple.addClock("Delay");
 		
 		simple.union("Mode", "Mode1", "Mode2");
 		simple.exclusion("Mode1", "Mode2");
 		
 		simple.delayFor("Delay", "Trigger", 1, -1, null);
 		simple.precedence("Mode", "Trigger");
-		simple.precedence("Trigger", "Mode");
-		simple.causality("Mode", "Delay");
+		simple.causality("Trigger", "Delay");
 	}
 	private static IUtility[] utilities = { 
 		new fr.kairos.timesquare.ccsl.simple.PrettyPrintUtility()
@@ -50,8 +51,8 @@ public class Lcmodes implements ISpecificationBuilder {
 		STSUtility sts = new STSUtility();
 		//ChocoInvariantHelper.activate(); // to reduce STS
 		JalinoptInvariantHelper.activate(); // to reduce STS
-		sts.setBackend(new fr.aoste.sync.gen.STStoDOT(), ".dot");
-		sts.setParam("folderName", "sts");
+		sts.setBackend(new STSJavaBackend());
+		sts.setParam("folderName", "src-gen/sts");
 		sts.treat(name, INSTANCE);
 	}
 }
