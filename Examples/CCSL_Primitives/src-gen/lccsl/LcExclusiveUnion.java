ec47ec9c0151bc6980759ece8bac836efa1d7ec2
fmallet <mallet.frederic@gmail.com>
2024-01-23 11:49:00 +0100

    Examples ABZ Challenge

--- Examples/CCSL_Primitives/src-gen/lccsl/LcExclusiveUnion.java
+++ Examples/CCSL_Primitives/src-gen/lccsl/LcExclusiveUnion.java
@@ -3,8 +3,6 @@ package lccsl;
 import fr.kairos.timesquare.ccsl.ISimpleSpecification;
 import fr.kairos.timesquare.ccsl.simple.IUtility;
 import fr.kairos.timesquare.ccsl.simple.ISpecificationBuilder;
-import fr.kairos.lightccsl.core.stepper.StepperUtility;
-import fr.kairos.lightccsl.sts.STSSolutionFinder;
 import fr.kairos.lightccsl.sts.STSUtility;
 //import fr.kairos.sts.pojo.choco.ChocoInvariantHelper;
 import fr.aoste.sync.ilp.JalinoptInvariantHelper;
@@ -34,11 +32,7 @@ public class LcExclusiveUnion implements ISpecificationBuilder {
 		for (IUtility u : utilities) {
 			u.treat(name, INSTANCE);
 		}
-		
-		StepperUtility exe = new StepperUtility(new STSSolutionFinder());
-		exe.setParam(StepperUtility.INTERACTIVE, false);
-		exe.setParam(StepperUtility.NB_STEPS, 20);
-		exe.treat(name, INSTANCE);
+		// no execution
 		
 		STSUtility sts = new STSUtility();
 		//ChocoInvariantHelper.activate(); // to reduce STS
